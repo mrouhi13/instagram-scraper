94b545552b8d982761820f5eab240780c7ff63b7
Majid Rouhi <mrouhi13@gmail.com>
2020-02-27 22:08:44 +0330

    Remove space

--- utils.py
+++ utils.py
@@ -7,7 +7,6 @@ def file_repetition_count(base_name, target_dir):
         if file.startswith(base_name):
             file_name, _ = file.split('.jpg')
             i = int(file_name.split('_')[-1])
-
     return i + 1
 
 
